83f8cb072c0e4ef800ef3d55a3674f3220db94e8
Ana Beatriz Dias <anadiasteotonio@usp.br>
2025-05-18 20:38:41 -0300

    Ajuste no estilo + criacao do componente headers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { ResultsComponent } from './results/results.component';
+import { HeaderComponent } from './header/header.component';
+
+
 
 export const routes: Routes = [
   { path: '', component: SearchComponent },
+  { path: 'header', component: HeaderComponent },
   { path: 'results', component: ResultsComponent },
 ];
